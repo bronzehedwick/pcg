5a47201243b2ebfd5da143eb345148092e94de35
Chris DeLuca <iam@chrisdeluca.me>
2020-02-02 14:52:38 -0500

    Make a base game web component
    Use web components to optimize and componentize our structure. Right now
    this encapsulates the positioning logic, and simplifies implementation.
    
    Next steps: something other than a test square as a graphic.

--- web/main.js
+++ web/main.js
@@ -2,19 +2,6 @@
   'use strict';
 
   /**
-   * Position game objects on the stage
-   * @return {void}
-   */
-  function layoutGameObjects() {
-    document
-      .querySelectorAll('.stage .game-object')
-      .forEach(element => {
-        element.style.top = element.dataset.positionY + 'px';
-        element.style.left = element.dataset.positionX + 'px';
-      });
-  }
-
-  /**
    * Render the given room
    * @param {string} id - The identifier for the room to render.
    * @return {void}
@@ -24,7 +11,6 @@
     const stage = document.getElementById('stage');
     stage.innerHTML = '';
     stage.appendChild(template);
-    layoutGameObjects();
   }
 
   // Add state of current active verb to the stage data attribute.
@@ -42,6 +28,44 @@
         .dataset
         .verbActive = element.id.split('verb-')[1];
     }, false);
+
+  /**
+   * `<game-object>` is the base object for all stage items.
+   * Pass in `x` and `y` values to position it on the canvas.
+   * If no value is passed, they default to `0`.
+   * Example:
+   *    <div class="stage">
+   *            <game-object x="200" y="800"></game-object>
+   * @customElement
+   */
+  class GameObject extends HTMLElement {
+    constructor() {
+      super();
+
+      const shadow = this.attachShadow({mode: 'open'});
+
+      const linkElement = document.createElement('link');
+      linkElement.setAttribute('rel', 'stylesheet');
+      linkElement.setAttribute('href', 'components/game-object.css');
+
+      const wrapperElement = document.createElement('div');
+      wrapperElement.setAttribute('class', 'wrapper');
+
+      const styleElement = document.createElement('style');
+      const y = this.getAttribute('y') ? this.getAttribute('y') : 0;
+      const x = this.getAttribute('x') ? this.getAttribute('x') : 0;
+      styleElement.textContent = `.wrapper {
+        top: ${y}px;
+        left: ${x}px;
+        background-color: ${this.getAttribute('color')};
+      }`;
+
+      shadow.appendChild(linkElement);
+      shadow.appendChild(styleElement);
+      shadow.appendChild(wrapperElement);
+    }
+  }
+  customElements.define('game-object', GameObject);
 
   renderRoom(document.querySelector('[data-initial]').id);
   // renderRoom('room-2');
